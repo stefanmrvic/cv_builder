9582ae1cb5803bd751d9cec98f52b58b105857d5
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-10-27 04:25:45 +0100

    add: collapsable functionality to Education component

--- src/components/EditControls/MainControls/Education.jsx
+++ src/components/EditControls/MainControls/Education.jsx
@@ -1,27 +1,39 @@
+import { useState, useRef } from 'react';
+
 import EducationItem from './EducationItem.jsx';
 
 import styles from './MainControls.module.css';
 
 export default function Education() {
+    const [isOpen, setIsOpen] = useState(false);
+    const btnContainerRef = useRef(null);
+
+    function handleCloseAnimation() {
+        btnContainerRef.current.setAttribute("class", `${styles.btnContainer} ${styles.closing}`)
+        btnContainerRef.current.onanimationend = () => setIsOpen(!isOpen);
+    }
+
     return (
         <div className={styles.educationContainer}>
-            <button className={styles.toggleBtn}>
+            <button className={`${styles.toggleBtn} ${isOpen ? styles.active : ''}`} onClick={isOpen ? handleCloseAnimation : () => setIsOpen(!isOpen)}>
                 <span className={`${styles.btnIcon} material-symbols-outlined`}>school</span>
                 <span className={styles.educationHeadline}>Education</span>
-                <span className="material-symbols-outlined">keyboard_arrow_down</span>
+                <span className={`material-symbols-outlined ${styles.arrowDown} ${isOpen ? styles.active : ''}`}>keyboard_arrow_down</span>
             </button>
 
-            <div className={styles.btnContainer}>
-                <EducationItem university='University of Barkeley' />
-                <EducationItem university='Masters University' />
+            {isOpen &&
+                <div className={`${styles.btnContainer} ${styles.opening}`} ref={btnContainerRef}>
+                        <EducationItem university='University of Barkeley' />
+                        <EducationItem university='Masters University' />
 
-                <div className={styles.addBtnContainer}>
-                    <button className={`${styles.addBtn} ${styles.btn}`}>
-                        <span className={`${styles.addBtnIcon} material-symbols-outlined`}>add</span>
-                        <span>Education</span>
-                    </button>
+                        <div className={styles.addBtnContainer}>
+                            <button className={`${styles.addBtn} ${styles.btn}`}>
+                                <span className={`${styles.addBtnIcon} material-symbols-outlined`}>add</span>
+                                <span>Education</span>
+                            </button>
+                        </div>
                 </div>
-            </div>
+            }
         </div>
     )
 }
--- src/components/EditControls/MainControls/MainControls.jsx
+++ src/components/EditControls/MainControls/MainControls.jsx
@@ -9,7 +9,6 @@ export default function MainControls() {
         <div className={styles.mainControls}>
             <PersonalDetails />
             <Education />
-            <Experience />
         </div>
     )
 }
