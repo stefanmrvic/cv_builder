67f133e2daaf24f5a1f70d193fcb6569cca794b4
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-11-02 01:17:20 +0100

    add: delete item functionality

--- src/components/EditControls/MainControls/EducationItem.jsx
+++ src/components/EditControls/MainControls/EducationItem.jsx
@@ -1,17 +1,15 @@
-import { useRef } from 'react';
-
 import styles from './MainControls.module.css';
 
 export default function EducationItem({itemID, setCVData, onClick, education}) {
-    const aRef = useRef(null);
+    const handleDeleteItem = (e) => {
+        e.stopPropagation();
 
-    const handleDeleteItem = () => {
         if (!itemID) throw new Error('itemID is undefined!');
 
         setCVData(draft => {
             const itemIndex = draft.education.findIndex(item => item.id === itemID);
 
-            if (!itemIndex) throw new Error('Item not found!');
+            if (itemIndex === undefined) throw new Error('Item not found!');
 
             draft.education.splice(itemIndex, 1);
         })
@@ -23,16 +21,16 @@ export default function EducationItem({itemID, setCVData, onClick, education}) {
         if (!itemID) throw new Error('itemID is undefined!');
 
        setCVData(draft => {
-        const item = draft.education.find(item => item.id === itemID);
+            const item = draft.education.find(item => item.id === itemID);
 
-        if (!item) throw new Error('Item not found!');
+            if (!item) throw new Error('Item not found!');
 
-        item.isVisible = !item.isVisible;
+            item.isVisible = !item.isVisible;
        })
     }
 
     return (
-        <a  onClick={onClick} className={styles.btn} ref={aRef}>
+        <a  onClick={onClick} className={`${styles.btn} ${styles.educationItem}`}>
             <span className={styles.btnText}>{education}</span>
             <div className={styles.showTextBtnContainer}>
                 <button onClick={handleDeleteItem}>
