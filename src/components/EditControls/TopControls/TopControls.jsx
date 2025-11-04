32d51239c5606fe01f3b17dfffc4f2433154a4aa
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-11-04 20:07:29 +0100

    add sumsum

--- src/components/EditControls/TopControls/TopControls.jsx
+++ src/components/EditControls/TopControls/TopControls.jsx
@@ -1,14 +1,47 @@
+import defaultCV from '../../../data/defaultCV.js'
+
 import styles from './TopControls.module.css'
 
-export default function TopControls() {
+export default function TopControls({cvData, setCVData}) {
+    const handleCVRemoval = () => {
+        const emptyCV = {
+            personalInfo: {
+                fullName: '',
+                birthDay: '', 
+                email: '', 
+                phone: '', 
+                location: '', 
+                linkedin: ''
+            },
+            workExperience: [],
+            skillsToolsInterests: {
+                skills: '',
+                tools: '',
+                interests: ''
+            },
+            education: []
+        }
+
+        setCVData(draft => {
+            return emptyCV;
+        })
+    }
+
+    const handleLoadDefaultCV = () => {
+
+        setCVData(draft => {
+            return defaultCV;
+        })
+    }
+
     return (
         <div className={styles.topControls}>
             <nav className={styles.topNavbar}>
-                <button className={styles.clearResume}>
+                <button className={styles.clearResume} onClick={handleCVRemoval}>
                     <span className="material-icons">delete</span>
                     <span>Clear Resume</span>
                 </button>
-                <button className={styles.loadExample}> 
+                <button className={styles.loadExample} onClick={handleLoadDefaultCV}> 
                     <span>Load Example</span>
                 </button>
             </nav>
