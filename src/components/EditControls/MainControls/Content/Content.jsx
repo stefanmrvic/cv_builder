6e9d418427c137479acb073793e80def991726e8
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-11-29 18:08:22 +0100

    add: content switching via sidebar

--- src/components/EditControls/MainControls/Content/Content.jsx
+++ src/components/EditControls/MainControls/Content/Content.jsx
@@ -3,9 +3,11 @@ import Experience from './Experience/Experience.jsx'
 import Education from './Education/Education.jsx';
 import SkillsToolsInterests from './SkillsToolsInterests/SkillsToolsInterests.jsx';
 
+import styles from './Content.module.css';
+
 export default function Content({data, setCVData}) {
     return (
-        <div >
+        <div className={styles.content}>
             <PersonalDetails data={data.personalInfo} setCVData={setCVData} />
             <Experience data={data.workExperience} setCVData={setCVData} />
             <SkillsToolsInterests data={data.skillsToolsInterests} setCVData={setCVData} />
