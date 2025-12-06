5b6b071d01a6504223b5e12a659a19d725db150d
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-12-06 01:46:22 +0100

    add: date formatting for CVPreview component

--- src/components/CV/Preview/WorkExperience/Company.jsx
+++ src/components/CV/Preview/WorkExperience/Company.jsx
@@ -4,25 +4,7 @@ import styles from './WorkExperience.module.css'
 
 export default function Company({company, bulletPoints}) {
     const visiblePositions = company.positions.filter(company => company.isVisible);
-
-    // console.log(visiblePositions)
     let firstVisiblePosition = visiblePositions[0];
-
-    const compareDates = (a, b) => {
-        console.log(a.endDate.valueAsNumber())
-        console.log(b.endDate.valueAsNumber())
-        if(a.endDate === 'Present' && b.endDate === 'Present') return 0
-        else if (a.endDate === 'Present') return 1;
-        else if (b.endDate === 'Present') return -1;
-
-        else if ((a.endDate.valueAsNumber - b.endDate.valueAsNumber) < 0) return -1;
-        else if ((a.endDate.valueAsNumber - b.endDate.valueAsNumber) > 0) return 1;
-        else return 0
-    }
-
-    // const sortedPositions = visiblePositions.sort(compareDates);
-    // console.log(sortedPositions)
-
 
     return (
         <div className={styles.companyContainer}>
--- src/components/CV/Preview/WorkExperience/Position.jsx
+++ src/components/CV/Preview/WorkExperience/Position.jsx
@@ -5,6 +5,20 @@ import styles from './WorkExperience.module.css';
 // Passes companyName and location so the first position can display company details above the job title,
 // since Position objects don't have access to their parent company's properties.
 export default function Position({isFirst, companyName, location, position, bulletPoints}) {
+    const formatDate = (dateStr) => {
+        const date = new Date(dateStr);
+        if (date === NaN) throw new Error('Invalid date!');
+
+        const dateArr = date.toDateString().split(' ');
+        const year = dateArr[3];
+        const month = dateArr[1];
+
+        return `${month}. ${year}`;
+    }
+
+    const formattedStartDate = formatDate(position.startDate);
+    const formattedEndDate = formatDate(position.endDate);
+
     return (
         <div className={styles.positionContainer}>
             <div className={styles.positionHeader}>
@@ -12,7 +26,7 @@ export default function Position({isFirst, companyName, location, position, bull
                     <>
                         <div className={styles.flexContainer}>
                             <p className={styles.companyName}>{companyName}</p>
-                            <p className={styles.startEndDate}>{position.startDate} - {position.currentlyEmployed ? 'Present' : position.endDate}</p>
+                            <p className={styles.startEndDate}>{formattedStartDate} - {position.currentlyEmployed ? 'Present' : formattedEndDate}</p>
                         </div>
                         <div className={styles.flexContainer}>
                             <p className={styles.jobTitle}>{position.title}</p>
@@ -22,7 +36,7 @@ export default function Position({isFirst, companyName, location, position, bull
                 ) : (
                         <div className={styles.flexContainer}>
                             <p className={styles.jobTitle}>{position.title}</p>
-                            <p className={styles.startEndDate}>{position.startDate} - {position.currentlyEmployed ? 'Present' : position.endDate}</p>
+                            <p className={styles.startEndDate}>{formattedStartDate} - {position.currentlyEmployed ? 'Present' : formattedEndDate}</p>
                         </div>
                     )
                 }
