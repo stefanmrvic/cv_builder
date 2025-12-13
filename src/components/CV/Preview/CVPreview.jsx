958c218b39c7be648d905fd2844df9386083a113
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-12-13 05:00:51 +0100

    style: add responsiveness

--- src/components/CV/Preview/CVPreview.jsx
+++ src/components/CV/Preview/CVPreview.jsx
@@ -15,22 +15,24 @@ export default function CVPreview({ data, order, bulletPoints }) {
             rerender when the item is removed in the runtime. Ref: https://github.com/diegomura/react-pdf/issues/3153#issuecomment-3124691739 
             P.S. Wrapping map function inside of render prop did not work, so this workaround had to be used as a last resort. */}
           <PDFDownloadLink 
-            key={Date.now()} className={styles.pdfDownloadBtn} document={<MyDocument data={data} order={order} bulletPoints={bulletPoints}/>} fileName='sumsum.pdf'
+            key={Date.now()} className={styles.pdfDownloadBtn} document={<MyDocument data={data} order={order} bulletPoints={bulletPoints}/>} fileName='luckyCV.pdf'
           >
             Download PDF
           </PDFDownloadLink>
 
-          <PersonalInfo data={data.personalInfo} />
+          <div className={styles.cvContentContainer}>
+            <PersonalInfo data={data.personalInfo} />
 
-          {order.map(item => {
-            if (item.id === 'workExperience') {
-              return <WorkExperience key={item.id} data={data.workExperience} bulletPoints={bulletPoints} />
-            } else if (item.id === 'skillsToolsInterests') {
-              return <SkillsToolsInterests key={item.id} data={data.skillsToolsInterests} bulletPoints={bulletPoints} />
-            } else {
-              return <Education key={item.id} data={data.education} />
-            }
-          })} 
+            {order.map(item => {
+              if (item.id === 'workExperience') {
+                return <WorkExperience key={item.id} data={data.workExperience} bulletPoints={bulletPoints} />
+              } else if (item.id === 'skillsToolsInterests') {
+                return <SkillsToolsInterests key={item.id} data={data.skillsToolsInterests} bulletPoints={bulletPoints} />
+              } else {
+                return <Education key={item.id} data={data.education} />
+              }
+            })}
+          </div>
         </div>
     )
 }
