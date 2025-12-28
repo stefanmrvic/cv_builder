ba85aad0b7c111981bbd20f22b106ab3b7550384
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2025-12-28 05:06:11 +0100

    fix: expensive PDF computations

--- src/components/CV/Preview/CVPreview.jsx
+++ src/components/CV/Preview/CVPreview.jsx
@@ -1,6 +1,6 @@
 import { useAppContext } from '../../../AppContext.jsx';
 
-import { PDFDownloadLink } from '@react-pdf/renderer';
+import { pdf } from '@react-pdf/renderer';
 import { MyDocument } from '../Generator/CVDocument.jsx';
 
 import PersonalInfo from './PersonalInfo/PersonalInfo.jsx';
@@ -10,38 +10,63 @@ import Education from './Education/Education.jsx'
 
 import styles from './CVPreview.module.css'
 
-export default function CVPreview() {
-    const { cvData, order, bulletPoints } = useAppContext();
+function DownloadButton({ cvData, order, bulletPoints, className }) {
+  const handleDownload = async () => {
+      // Generates PDF only when the download button is clicked in order to avoid expensive PDF computations
+      const blob = await pdf(
+        <MyDocument data={cvData} order={order} bulletPoints={bulletPoints} />
+      ).toBlob();
 
-    return (
-      <div className={styles.cvContainer}>
-        {/* Using key as Date.now() because there is a bug in react-pdf library which causes <PDFDownloadLink /> component not to
-          rerender when the item is removed in the runtime. Ref: https://github.com/diegomura/react-pdf/issues/3153#issuecomment-3124691739 
-          P.S. Wrapping map function inside of render prop did not work, so this workaround had to be used as a last resort. */}
-        <PDFDownloadLink 
-          key={Date.now()} 
-          className={styles.pdfDownloadBtn} 
-          // Have to pass down the props because Context API is not working with react-pdf library... (╯°□°)╯︵ ┻━┻
-          // GitHUb issue is still open: https://github.com/diegomura/react-pdf/issues/522
-          document={<MyDocument data={cvData} order={order} bulletPoints={bulletPoints} />} 
-          fileName='luckyCV.pdf'
-        >
+      // Creating download link
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+
+      link.href = url;
+      link.download = 'luckyCV.pdf';
+      link.click();
+
+      // Releasing object to avoid memory leak
+      URL.revokeObjectURL(url);
+  };
+
+  return (
+      <button className={className} onClick={handleDownload}>
           Download PDF
-        </PDFDownloadLink>
+      </button>
+  );
+}
 
-        <div className={styles.cvContentContainer}>
-          <PersonalInfo />
+export default function CVPreview() {
+  const { cvData, order, bulletPoints } = useAppContext();
 
-          {order.map(item => {
-            if (item.id === 'workExperience') {
-              return <WorkExperience key={item.id} />
-            } else if (item.id === 'skillsToolsInterests') {
-              return <SkillsToolsInterests key={item.id} />
-            } else {
-              return <Education key={item.id} />
-            }
-          })}
-        </div>
+  return (
+    <div className={styles.cvContainer}>
+      {/* Using key as Date.now() because there is a bug in react-pdf library which causes <PDFDownloadLink /> component not to
+        rerender when the item is removed in the runtime. Ref: https://github.com/diegomura/react-pdf/issues/3153#issuecomment-3124691739 
+        P.S. Wrapping map function inside of render prop did not work, so this workaround had to be used as a last resort. */}
+      <DownloadButton 
+        key={Date.now()} 
+        className={styles.pdfDownloadBtn} 
+        cvData={cvData}
+        order={order}
+        bulletPoints={bulletPoints}
+      >
+        Download PDF
+      </DownloadButton>
+
+      <div className={styles.cvContentContainer}>
+        <PersonalInfo />
+
+        {order.map(item => {
+          if (item.id === 'workExperience') {
+            return <WorkExperience key={item.id} />
+          } else if (item.id === 'skillsToolsInterests') {
+            return <SkillsToolsInterests key={item.id} />
+          } else {
+            return <Education key={item.id} />
+          }
+        })}
       </div>
-    )
+    </div>
+  )
 }
